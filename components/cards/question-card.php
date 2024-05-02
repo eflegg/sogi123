b03a5bd92ac1fb96b341ad9ebc0d0a84520aa100
Julie Rankin <hello@julierankin.com>
2024-05-02 13:52:43 -0700

    hero adjustments for long title

--- components/cards/question-card.php
+++ components/cards/question-card.php
@@ -3,7 +3,8 @@
 <?php $question = $the_question['question'];?>
 <?php $answer = $the_question['answer'];?>
 
-<li tabIndex="0" class="card question-card card-color">
+<li class="card question-card card-color">
+    <a href="<?php the_permalink(); ?>">
     <div  class="question--text">
         <span class="the-question">
             "<?php echo $question ?>"
@@ -11,7 +12,8 @@
        <span class="the-answer">
        <?php echo $answer;?>
        </span>
-        <a tabIndex="0" class="continue-reading" href="<?php the_permalink(); ?>">Continue reading &#10142;</a>
-    </div>    
+        <span class="continue-reading">Continue reading &#10142;</span>
+    </div>  
+    </a>  
 </li>
 <?php endif;?>
