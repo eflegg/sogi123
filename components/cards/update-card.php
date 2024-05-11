014f4eb5cb2abbed096533e9a2ea957e2314f941
Erin Flegg <ehflegg@gmail.com>
2024-05-10 19:10:37 -0700

    finish ups, see spreadsheet for completed items

--- components/cards/update-card.php
+++ components/cards/update-card.php
@@ -1,15 +1,19 @@
 <li class="update-card-container  update-color  single-slide">
+<a class="card-link" href="<?php the_permalink(); ?>">
     <div class="update-card">
-        <div class="tab"></div>
-        <div class="inner">
-            <p class="post-date"><?php echo get_the_date();?></p>
-            <h4 class="post-title">
-                <?php the_title(); ?>
-            </h4>
-            <?php the_excerpt(); ?>
-            <div class="content">
-               <a class="learn-button" href="<?php the_permalink(); ?>">Learn More</a>
-            </div>
-        </div>  
-    </div>
-</li>
+          
+            <div class="tab"></div>
+            <div class="inner">
+                <p class="post-date"><?php echo get_the_date();?></p>
+                <h4 class="post-title">
+                    <?php the_title(); ?>
+                </h4>
+                <?php the_excerpt(); ?>
+                <div class="content">
+                <p class="learn-button" >Learn More</p>
+                </div>
+            </div>  
+
+        </div>
+</a>
+    </li>
--- components/sidebars/share-sidebar.php
+++ components/sidebars/share-sidebar.php
@@ -7,20 +7,20 @@ $sidebarText = $sidebarSettings['share_text'];
 <div class="shape "></div>
 <div class="share-sidebar <?php echo $sidebarType;?>">
     <div class="share--inner">
-        <h3 class="fade-me"><?php echo $sidebarTitle;?></h3>
-        <p class="fade-me"><?php echo $sidebarText;?></p>
-        <p class="share-this fade-me">Share this with your network:</p>
+        <h3 class=""><?php echo $sidebarTitle;?></h3>
+        <p class=""><?php echo $sidebarText;?></p>
+        <p class="share-this ">Share this with your network:</p>
 
 
 <div class="socials display-flex a2a_kit a2a_kit_size_32 a2a_default_style">
-    <a class="a2a_button_facebook fade-me">
+    <a class="a2a_button_facebook ">
 	<div class="social sidebar">
 		<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
 			<path d="M13.91 0C6.23 0 0 6.23 0 13.91C0 21.59 6.23 27.82 13.91 27.82C21.59 27.82 27.82 21.59 27.82 13.91C27.82 6.23 21.59 0 13.91 0ZM16.8 13.67C16.78 13.86 16.66 13.86 16.53 13.86C16.01 13.86 15.48 13.86 14.92 13.86V20.55H12.21V13.9H10.84V11.63H12.19C12.19 11.47 12.19 11.35 12.19 11.22C12.22 10.5 12.2 9.77 12.31 9.06C12.44 8.17 13.03 7.63 13.89 7.41C14.22 7.33 14.57 7.29 14.92 7.28C15.59 7.26 16.26 7.28 16.97 7.28V9.61C16.53 9.61 16.08 9.61 15.62 9.61C15.05 9.61 14.94 9.72 14.93 10.28C14.93 10.7 14.93 11.11 14.93 11.53C14.93 11.55 14.94 11.57 14.96 11.61H16.99C16.93 12.32 16.86 12.99 16.8 13.67Z" fill="#FFC700"/>
 			</svg>
 		</div>
 	</a>
-    <a class="a2a_button_linkedin fade-me">
+    <a class="a2a_button_linkedin ">
 		<div class="social sidebar">
 		<svg width="28" height="28" viewBox="0 0 28 28"  version="1.1" id="linkedin-circle" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
 	 y="0px" viewBox="0 0 28 28" style="enable-background:new 0 0 28 28;" xml:space="preserve">
@@ -35,7 +35,7 @@ $sidebarText = $sidebarSettings['share_text'];
 		</div>
 	</a>
    
-    <a class="a2a_dd fade-me" href="https://www.addtoany.com/share"></a>
+    <a class="a2a_dd " href="https://www.addtoany.com/share"></a>
 </div>
      
 
